cc3ef57428e913aa9817f171ee5c37dd6bc2a55f
andy_hardy <aohardy@gmail.com>
2019-04-15 18:45:40 -0600

    added AJAX calls for buttons

--- assets/javascript/giphy-app.js
+++ assets/javascript/giphy-app.js
@@ -16,6 +16,27 @@ function newButton(value) {
     $(".button-land").append(button);
 }
 
+function makeGifDiv(data) {
+    // make the image tag for each data value
+    var gifImg = $("<img>");
+    gifImg.attr({
+        src: data.images.fixed_height_still.url,
+        "data-animate":  data.images.fixed_height.url,
+        "data-still": data.images.fixed_height_still.url,
+        "data-state": "still"
+    });
+    // make the <p> element to put the rating info in
+    var ratingP = $("<p>");
+    ratingP.addClass("gif-rating");
+    ratingP.text(data.rating.toUpperCase());
+    // make a <div> to wrap the img and p in
+    var gifDiv = $("<div>");
+    gifDiv.addClass("gif-div");
+    gifDiv.append(gifImg, ratingP);
+    // prepend the div with rating and image to the page at the div w/ class gif-tag
+    $(".gif-land").prepend(gifDiv);
+}
+
 // ------------------------------ Objects Constructor and Global functions --------------------
 
 // make the buttons from the initial array
@@ -37,7 +58,45 @@ $("#search-button").on("click", function() {
         $("#search-field").val("");
     }
 
+    // search the GIPHY api for the value
+    var apiKey = "7yoI0Q9V3e6BZA3icwdVrvDDVdqqaLqt";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key="+ apiKey + "&q="+ value + "&limit=10&offset=0&rating=R&lang=en";
+    // grab some info from the giphy API
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then( function (response) {
+        console.log(response);
+        response.data.forEach( function (data) {
+            // make the image div for each data value
+            makeGifDiv(data);
+        });
+    });
 });
+// onclick function for the buttons
+$("body").on("click", ".gif-search", function() {
+
+    // save the value of the form to a variable, as long as the value is a string of some kind and then make a new button
+    var value = $(this).attr("data-name");
+    console.log(value);
+
+    // search the GIPHY api for the value
+    var apiKey = "7yoI0Q9V3e6BZA3icwdVrvDDVdqqaLqt";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key="+ apiKey + "&q="+ value + "&limit=10&offset=0&rating=R&lang=en";
+    // grab some info from the giphy API
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then( function (response) {
+        console.log(response);
+        response.data.forEach( function (data) {
+            // make the image div for each data value
+            makeGifDiv(data);
+        });
+    });
+})
+
+
 
 
 
